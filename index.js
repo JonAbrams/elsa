6f399290f6756895dd8892690252972a58340b1f
Jon Abrams <jon@jonabrams.com>
2017-04-22 17:11:40 -0700

    Use memberExpression to construct Object.freeze

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ module.exports = function elsa({ types: t }) {
   return {
     visitor: {
       'ObjectExpression|ArrayExpression'(path) {  // select new object and array literals
-        var props = path.node.properties;
         if (!t.isCallExpression(path.parent)) {   // Exclude objects passed directly to func calls
                                                   // otherwise causes infinite recursion
           path.replaceWith(                       // wrap in Onject.freeze
-            t.callExpression(t.identifier('Object.freeze'), [path.node])
+            t.callExpression(
+              t.memberExpression(t.identifier('Object'), t.identifier('freeze')),
+              [path.node]
+            )
           );
         }
       },
