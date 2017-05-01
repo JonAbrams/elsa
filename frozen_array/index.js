01c3880529e8891dbdf201fd7eeb5f31da457cb7
Jon Abrams <jon@jonabrams.com>
2017-04-30 21:58:20 -0700

    Add copyWithin

--- frozen_array/index.js
+++ frozen_array/index.js
@@ -1,4 +1,4 @@
-const FrozenArray = module.exports = class FrozenArray extends Array {
+class FrozenArray extends Array {
   constructor(...items) {
     // Note: Cannot just do `super(...items)`` because if there's one item, it's treated as
     // desired size instead of a single array item.
@@ -27,12 +27,14 @@ const FrozenArray = module.exports = class FrozenArray extends Array {
     arr.splice(...args);
     return new FrozenArray(...arr);
   }
-};
+}
 
-['sort', 'reverse', 'fill', 'splice', 'unshift', 'push'].forEach(method => {
-  FrozenArray.prototype[method] = function(...args) {
+['sort', 'reverse', 'fill', 'splice', 'unshift', 'push', 'copyWithin'].forEach(method => {
+  FrozenArray.prototype[method] = function (...args) { // eslint-disable-line func-names
     const arr = new Array(...this);
     arr[method](...args);
     return new FrozenArray(...arr);
   };
 });
+
+module.exports = FrozenArray;
